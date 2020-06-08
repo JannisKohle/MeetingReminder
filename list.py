2547e446d616ba197eaa22176e116ddc46d319ae
JannisKohle <Jannis.Kohle@icloud.com>
2020-06-08 19:51:28 +0200

    changed list.py

--- list.py
+++ list.py
@@ -2,5 +2,9 @@ import json
 
 with open("meetings.json", "r+") as f:
     content = json.load(f)
+    if content == {}:
+        print("No meetings")
+        return
+
     for k, v in zip(list(content.keys()), list(content.values())):
         print(f"- meeting {k} at time {v}")
